bca56e1740ff9fcde74f5f7206edc37ab098d114
Qasim Laghari <go.jets.laghari@gmail.com>
2018-10-19 17:31:15 -0500

    Add video components

--- src/index.js
+++ src/index.js
@@ -3,31 +3,34 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyD3o7QSIZ4qAYJ_LCDnlu8ymyEE0QQLHh8';
 
-YTSearch(
-  {
-    key: API_KEY,
-    term: 'surfboards'
-  },
-  data => {
-    this.setState({
-      videos: data
-    });
-  }
-);
-// Create a new component
+// Create a new class component
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { videos: [] };
+
+    YTSearch(
+      {
+        key: API_KEY,
+        term: 'surfboards'
+      },
+      data => {
+        this.setState({
+          videos: data
+        });
+      }
+    );
   }
 
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

52cad34f121697a80fc267faf87be3027ebd17ff
Qasim Laghari <go.jets.laghari@gmail.com>
2018-10-19 17:49:58 -0500

    Update video component to display thumbnail

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class App extends Component {
     YTSearch(
       {
         key: API_KEY,
-        term: 'surfboards'
+        term: 'kiki challenge'
       },
       data => {
         this.setState({
